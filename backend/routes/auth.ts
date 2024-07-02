00c24e74f4269986f0a317b6c26d3700a3a38ceb
Fahad Khan <kanlifahad@gmail.com>
2024-07-02 11:45:27 -0500

    route creation

--- backend/routes/auth.ts
+++ backend/routes/auth.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import { signUpRouter } from './signUp';
 import { signInRouter } from './signIn';
+import { resetPasswordRouter } from './resetPassword';
 
 export const authRouter = Router();
 
 authRouter.use('/sign-up', signUpRouter);
 authRouter.use('/sign-in', signInRouter);
+authRouter.use('/reset-password', resetPasswordRouter);
